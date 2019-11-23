2e5b793af1da8f9ef2352b384d60e9a88a462eab
蒋创事 <jiangchuangshi@znlhzl.com>
2019-11-23 10:23:46 +0800

    引入和使用element-ui

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <el-select v-model="city">
+    <el-option v-for="item in cities" :key="item.value" :label="item.label" :value="item.value">
+      <span style="float: left">{{ item.label }}</span>
+      <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
+    </el-option>
+  </el-select>
 </template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    return {
+      city: "",
+      cities: [
+        {
+          value: "Beijing",
+          label: "北京"
+        },
+        {
+          value: "Shanghai",
+          label: "上海"
+        },
+        {
+          value: "Nanjing",
+          label: "南京"
+        },
+        {
+          value: "Chengdu",
+          label: "成都"
+        },
+        {
+          value: "Shenzhen",
+          label: "深圳"
+        },
+        {
+          value: "Guangzhou",
+          label: "广州"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
